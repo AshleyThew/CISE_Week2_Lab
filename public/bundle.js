e3a46463607ef26e6bf978563e09092f5f247df5
Dablakbandit <ashle.thew@gmail.com>
2021-03-18 06:11:24 +1300

    Serve using Express

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";try{document.getElementById("day").innerText=((e=new Date)=>["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][e.getDay()])()}catch(e){}})();
